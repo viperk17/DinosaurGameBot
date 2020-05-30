ee4eeb4959f98c8c2c0bc18f86514e8b7ff5751e
Prashant Singh <pk.airsqn@protonmail.com>
2020-05-30 11:25:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+import pyautogui # pip install pyautogui
+from PIL import Image, ImageGrab # pip install pillow
+# from numpy import asarray
+import time
+
+def hit(key):
+    pyautogui.keyDown(key)
+    return
+
+def isCollide(data):
+    # Drawing the rectangle to dodge the birds
+    for i in range(300, 415):
+        for j in range(410, 563):
+            if data[i, j] < 100:
+                hit("down")
+                return
+
+    # Drawing rectangle to jump the cactus
+    for i in range(300, 415):
+        for j in range(563, 650):
+            if data[i, j] < 100:
+                hit("up")
+                return
+    return
+
+if __name__ == "__main__":
+    print("Hey... Dinosaur game is about to start in 3 seconds")
+    time.sleep(2)
+    # hit('up') 
+
+    while True:
+        image = ImageGrab.grab().convert('L')  # Convert into greyscale image
+        data = image.load() # Loads the image
+        isCollide(data)
+            
+        
+        '''
+        for i in range(275, 325):
+            for j in range(563, 650):
+                data[i, j] = 0
+        
+        for i in range(250, 300):
+            for j in range(410, 563):
+                data[i, j] = 171
+
+        image.show()
+        break
+      '''
+
